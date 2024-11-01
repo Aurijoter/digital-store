03ceed83b349c4c8785e27ec291e8a1adc37f7b1
Matheus Rodrigues <39568326+Aurijoter@users.noreply.github.com>
2024-11-01 13:55:51 -0300

    First commit
    Created Project, created style files and install initial dependencies

---
+++ src/Style/Constants.jsx
@@ -0,0 +1,16 @@
+// Colors
+export const primary   = "#C92071"
+export const secondary = "#B5B6F2"
+export const tertiary  = "#991956"
+export const error     = "#EE4266"
+export const success   = "#52CA76"
+export const warning   = "#F6AA1C"
+
+//Grayscale
+export const dark_gray    = "#1F1F1F"
+export const dark_gray_2  = "#474747"
+export const dark_gray_3  = "#666666"
+export const light_gray   = "#8F8F8F"
+export const light_gray_2 = "#CCCCCC"
+export const light_gray_3 = "#F5F5F5"
+export const white        = "#FFFFFF"
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+import ResetStyle from './Style/ResetStyle.jsx';
+import GlobalStyle from './Style/GlobalStyle.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ResetStyle />
+    <GlobalStyle />
+    <App />
+  </StrictMode>,
+)
+
